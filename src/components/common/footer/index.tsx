319d34fb2e0b6fc4b4179cf115f71eb19608260c
sub1n17 <sub1n17@naver.com>
2026-02-10 08:40:24 +0900

    feat: 푸터 구현

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link';
+import style from './styles.module.css';
+
 export default function Footer() {
     return (
-        <>
-            <div>푸터푸터</div>
-        </>
+        <footer className={style.footer}>
+            <div className={style.footer_inner}>
+                <div className={style.footer_content}>
+                    <Link className={style.email} href={'mailto:subin17@naver.com'}>
+                        sub1n17@naver.com
+                    </Link>
+                    <nav className={style.footer_nav}>
+                        <ul>
+                            <li>
+                                <Link href={'/projects'} data-text="Projects">
+                                    Projects
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={'/about'} data-text="About">
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={'https://github.com/sub1n17'} data-text="Github">
+                                    Github
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <p className={style.copyright}>© 2026 Choi Subin.</p>
+            </div>
+        </footer>
     );
 }
