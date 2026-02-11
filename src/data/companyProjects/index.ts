5fa91cdb75ca1080bbec0950b3750b7ab08b43ad
sub1n17 <sub1n17@naver.com>
2026-02-11 09:04:18 +0900

    chore: 데이터 구조 수정

--- src/data/companyProjects/index.ts
+++ src/data/companyProjects/index.ts
@@ -4,7 +4,12 @@ export const companyProjects = [
         title: '우진물산 공식 홈페이지',
         summary: 'HTML, CSS, JavaScript로 직접 제작한 기업 홈페이지',
         role: '메인·상세 페이지 퍼블리싱',
-        techStack: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+        skills: [
+            {
+                type: 'Skills',
+                skill: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+            },
+        ],
         thumbnail: '/images/project_c1.png',
         link: 'http://woojinmulsan.co.kr',
         type: 'Company Website',
@@ -14,7 +19,12 @@ export const companyProjects = [
         title: '엔초이스 공식 홈페이지',
         summary: 'fullPage.js를 사용해서 제작한 식품 사이트',
         role: '메인·상세 페이지 퍼블리싱',
-        techStack: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+        skills: [
+            {
+                type: 'Skills',
+                skill: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+            },
+        ],
         thumbnail: '/images/project_c2.png',
         link: 'https://anchoice2023.mycafe24.com',
         type: 'Brand Website',
@@ -24,7 +34,12 @@ export const companyProjects = [
         title: '대한물산 공식 홈페이지',
         summary: '기존 기업 사이트를 리뉴얼하여 구조 개선과 퍼블리싱을 담당',
         role: '메인·상세 페이지 퍼블리싱',
-        techStack: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+        skills: [
+            {
+                type: 'Skills',
+                skill: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+            },
+        ],
         thumbnail: '/images/project_c3.png',
         link: 'https://daehanmulsan2025.mycafe24.com',
         type: 'Company Website',
@@ -34,7 +49,12 @@ export const companyProjects = [
         title: 'FOS KOREA 공식 홈페이지',
         summary: '카페24 환경에서 배포·운영한 기업 공식 웹사이트',
         role: '상세페이지 퍼블리싱',
-        techStack: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+        skills: [
+            {
+                type: 'Skills',
+                skill: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+            },
+        ],
         thumbnail: '/images/project_c4.png',
         link: 'https://foskorea.net',
         type: 'Corporate Website',
